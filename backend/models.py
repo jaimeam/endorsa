210b97b8431cb107941256831354828d3db7f5d8
Jaime Adroher <jaime.adroher@gmail.com>
2020-04-10 12:30:08 +0200

    Add Flask db migrate, create Procfile for Heroku

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 database_name = "endorsa"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
@@ -12,6 +13,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
+    migrate = Migrate(app, db)
     db.init_app(app)
     db.create_all()
 
